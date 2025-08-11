31e31f11af0e7b3ce1a568cec10fd8cbfdc24d75
Hkhan2712 <nguynkit3107@gmail.com>
2025-08-11 16:52:27 +0700

    Added: Data and Image for demo UI

---
+++ public/data/serviceItem.js
@@ -0,0 +1,18 @@
+// data/serviceItems.js
+export const serviceItems = [
+  {
+    icon: "/icons/shipping.svg",
+    title: "Complimentary Shipping",
+    description: "Enjoy free shipping on U.S. orders over $100."
+  },
+  {
+    icon: "/icons/crafted.svg",
+    title: "Consciously Crafted",
+    description: "Designed with you and the planet in mind."
+  },
+  {
+    icon: "/icons/location.svg",
+    title: "Come Say Hi",
+    description: "We have 11 stores across the U.S."
+  }
+];
