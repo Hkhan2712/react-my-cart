0b4b11806a637e55e8daab87eba40c5ceaa807fa
Hkhan2712 <nguynkit3107@gmail.com>
2025-08-11 16:54:31 +0700

    Modified: Button in common component

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import styles from './Button.module.css';
-import clsx from 'clsx'; 
+import clsx from 'clsx';
 
-const Button = ({ children, variant = 'primary', rounded = false, ...props }) => {
+const Button = ({
+    children,
+    variant = 'primary',
+    rounded = false,
+    href,
+    className,
+    ...props
+}) => {
     const buttonClasses = clsx(
         styles.btn,
-        styles[variant],   
-        rounded && styles.rounded
+        styles[variant],
+        rounded && styles.rounded,
+        className
     );
+
+    if (href) {
+        return (
+            <a href={href} className={`${buttonClasses} text-decoration-none`} {...props}>
+                {children}
+            </a>
+        );
+    }
 
     return (
         <button className={buttonClasses} {...props}>
