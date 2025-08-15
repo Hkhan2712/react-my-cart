19b225a513f0b891efc54ffdd0a8dd9d6a84bfb2
Hkhan2712 <nguynkit3107@gmail.com>
2025-08-15 16:57:09 +0700

    Card component for categories

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,14 +1,11 @@
 import styles from './Card.module.css'
 
-const Card = ({title, imageUrl}) => {
-    return (
-        <div className="card h-100">
-            <img src={imageUrl} className="cardImage" alt={title} />
-            <div className="card-body text-center">
-                <h6 className={`${styles.cardTitle}`}>{title}</h6>
-            </div>
+const Card = ({name, imageUrl}) => 
+    <div className="card h-100">
+        <img src={imageUrl} className="cardImage" alt={name} />
+        <div className="card-body text-center">
+            <h6 className={`${styles.cardTitle}`}>{name}</h6>
         </div>
-    )
-}
+    </div>
 
 export default Card;
